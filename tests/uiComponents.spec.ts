44eabc3b344e99bc4c797a5b3099e9e383ae50fc
maksim.nikiforov <pusyash91@yandex.ru>
2024-02-17 17:18:39 -0300

    Leson 34. Radio buttons completed

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -28,4 +28,22 @@ test.describe ('Form Layouts page', () => {
         await expect(usingTheGridEmailInput).toHaveValue("test2@test.com")
 
     })
+
+    test ('Radio Buttons', async ({page}) => {
+        const usingTheGridForm = page.locator('nb-card', {hasText: "Using the Grid"})
+
+        //await usingTheGridForm.getByLabel('Option 1').check({force: true})
+        await usingTheGridForm.getByRole('radio', {name: "Option 1"}).check({force: true})
+
+        // generic assertions
+        const radioStatus = await usingTheGridForm.getByRole('radio', {name: 'Option 1'}).isChecked()
+        expect(radioStatus).toBeTruthy()
+
+        //locator assertion
+        expect(usingTheGridForm.getByRole('radio', {name: 'Option 1'})).toBeChecked()
+
+        await usingTheGridForm.getByRole('radio', {name: "Option 2"}).check({force: true})
+        expect(await usingTheGridForm.getByRole('radio', {name: 'Option 1'}).isChecked()).toBeFalsy()
+        expect(await usingTheGridForm.getByRole('radio', {name: 'Option 2'}).isChecked()).toBeTruthy()
+    })
 })
