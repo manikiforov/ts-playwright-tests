0fa727dc5164ff1e3a62fabe6da94274574d9243
maksim.nikiforov <pusyash91@yandex.ru>
2024-02-17 18:49:46 -0300

    Lesson 38. "Dialog boxes" completed

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -109,3 +109,16 @@ test ('Tooltips', async({page}) => {
 
     await expect(tooltip).toEqual('This is a tooltip')
 })
+
+test ('Dialog Boxes', async({page}) => {
+    await page.getByText('Tables & Data').click()
+    await page.getByText('Smart Table').click()
+
+    page.on('dialog', dialog => {
+        expect(dialog.message()).toEqual('Are you sure you want to delete?')
+        dialog.accept()
+    })
+
+    await page.getByRole('table').locator('tr', {hasText: "mdo@gmail.com"}).locator('.nb-trash').click()
+    await expect(page.locator('tabe tr').first()).not.toHaveText('mdo@gmail.com')
+})
