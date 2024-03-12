cfdc249d2b25dc809ede2da99c3657ed705e85b2
maksim.nikiforov <pusyash91@yandex.ru>
2024-03-12 18:14:44 -0300

    "Lesson 41. Date Picker pt. 1" completed

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -172,3 +172,16 @@ test ('Web Tables', async({page}) => {
     }
 
 })
+
+test ('datepicker', async({page}) => {
+
+    await page.getByText('Forms').click()
+    await page.getByText('Datepicker').click()
+
+    const calendarInputField = page.getByPlaceholder('Form Picker')
+    await calendarInputField.click()
+
+    await page.locator('[class="day-cell ng-star-inserted"]').getByText('1', {exact: true}).click()
+    await expect(calendarInputField).toHaveValue('Mar 1, 2024')
+
+})
