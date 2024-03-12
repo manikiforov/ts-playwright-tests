3c5000f8892283b5371b2d27da0d08aca0bdbaac
maksim.nikiforov <pusyash91@yandex.ru>
2024-03-12 17:56:27 -0300

    "Lesson 40. Web Tables pt. 2" completed

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -144,4 +144,31 @@ test ('Web Tables', async({page}) => {
     await page.locator('.nb-checkmark').click()
     await expect(targetRowById.locator('td').nth(5)).toHaveText('test@test.com')
 
+    // 03 Test filter on the table
+    const ages = ['20', '30', '40', '200']
+
+    for (let age of ages) {
+
+        await page.locator('input-filter').getByPlaceholder('Age').clear()
+        await page.locator('input-filter').getByPlaceholder('Age').fill(age)
+        await page.waitForTimeout(500)
+        const ageRows = page.locator('tbody tr')
+
+        for (let row of await ageRows.all()) {
+
+            const cellValue = await row.locator('td').last().textContent()
+
+            if (age == '200') {
+                
+                expect(await page.getByRole('table').textContent()).toContain('No data found')
+            } else {
+                
+                expect(cellValue).toEqual(age)
+
+            }
+
+        }
+
+    }
+
 })
