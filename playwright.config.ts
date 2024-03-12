7dc81e2c412a4d22a3eb8be53a10989ae2f8b267
maksim.nikiforov <pusyash91@yandex.ru>
2024-03-12 19:24:52 -0300

    "Lesson 44. DragNDrop with iFrames" completed

--- playwright.config.ts
+++ playwright.config.ts
@@ -34,7 +34,7 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     //actionTimeout: 5000,
-    navigationTimeout: 10000
+    navigationTimeout: 30000
   },
 
   /* Configure projects for major browsers */
